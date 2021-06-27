e0cf97f612fc89f404d125b38eea0967227f5ba8
CYderekyeh <86555741+CYderekyeh@users.noreply.github.com>
2021-06-27 20:48:41 +0800

    DP method

--- PDAO 2017 P2.py
+++ PDAO 2017 P2.py
@@ -1,10 +1,7 @@
 k = int(input())
 a = list(map(int, input().split()))
-y = [0]
+dp = [0]  * (k+1)
 for i in range (1, k+1):
-    if a[i - 1] == i:
-        take = 2
-    else:
-        take = y[i-1] - y[a[i-1]-1] + 2
-    y.append(take + y[i-1])
-print(y[k] % 1000000007)
+    dp[i] += dp[i-1] - dp[a[i-1]-1] + 2
+    dp[i] += dp[i-1]
+print(dp[k] % 1000000007)
