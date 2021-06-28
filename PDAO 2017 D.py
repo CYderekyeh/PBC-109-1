7e0523d5e59e527ad44603ec5cec665ac4cb1e80
CYderekyeh <86555741+CYderekyeh@users.noreply.github.com>
2021-06-28 20:42:42 +0800

    Update PDAO 2017 D.py

--- PDAO 2017 D.py
+++ PDAO 2017 D.py
@@ -1,12 +1,12 @@
 s1, s2 = map(str, input().split())
 m = len(s1) 
 n = len(s2)
-dp = [[0] * (n+1)] * (m+1)
+dp = [[0 for x in range(n+1)] for x in range(m+1)]
 for i in range(m+1):
     for j in range(n+1):
-        if (i == 0 or j == 0):
+        if i == 0 or j == 0:
             dp[i][j] = 0
-        elif (s1[i-1] == s2[j-1]):
+        elif s1[i-1] == s2[j-1]:
             dp[i][j] = dp[i-1][j-1] + 1
         else:
             dp[i][j] = max(dp[i][j-1], dp[i-1][j])
